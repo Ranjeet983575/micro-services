f9c8bba8ef515806fde5f10818840fce0df23dc4
rkprajapati404 <rkprajapati404@gmail.com>
2025-04-18 17:02:26 +0800

    create eruka seerver and API gateway

--- api/src/main/java/com/ms/gateway/api/ApiApplication.java
+++ api/src/main/java/com/ms/gateway/api/ApiApplication.java
@@ -1,4 +1,4 @@
-package com.arg.user.user;
+package com.ms.gateway.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class UserApplication {
+public class ApiApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(UserApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
 
 }
