02cf70eceba12d252f48cc15d47e7557cbcf4313
rkprajapati404 <rkprajapati404@gmail.com>
2025-07-25 23:37:33 +0800

    add exception handler

--- user/src/main/java/com/arg/user/user/exception/ExceptionHandler.java
+++ user/src/main/java/com/arg/user/user/exception/ExceptionHandler.java
@@ -3,9 +3,9 @@ package com.arg.user.user.exception;
 import com.arg.user.user.dto.ExceptionResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ExceptionHandler {
 
     @org.springframework.web.bind.annotation.ExceptionHandler(ResourceNotFoundException.class)
